1b2a3c10d3d780ff2c309e32e7c9e410480f2109
ghanitutorsbot <60375240+ghanitutorsbot@users.noreply.github.com>
2022-07-18 08:07:58 +0530

    Update main.tf

--- 1_Ec2/main.tf
+++ 1_Ec2/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.13.0"
-    }
-  }
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0022f774911c1d690"
@@ -21,36 +8,3 @@ resource "aws_instance" "app_server" {
   }
 }
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "my-tf-test-bucket1232133434"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-data "aws_ami" "amazon" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20210701.0-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["137112412989"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami    = data.aws_ami.amazon.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "WebMachine"
-  }
-}
