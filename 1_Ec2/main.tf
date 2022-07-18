ccb782b2b9fd26c523c612f6cffbe2b08f53857d
lokesh <lokesh@gmail.com>
2022-07-18 08:14:33 +0530

    added

--- 1_Ec2/main.tf
+++ 1_Ec2/main.tf
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> d19231927ffeca75f615117b05c4d368c03749fc
 resource "aws_instance" "app_server" {
   ami           = "ami-0022f774911c1d690"
   instance_type = "t2.micro"
--- 1_Ec2/providers.tf
+++ 1_Ec2/providers.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 terraform {
   required_providers {
     aws = {
@@ -12,18 +11,3 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
-=======
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.22.0"
-    }
-  }
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
->>>>>>> d19231927ffeca75f615117b05c4d368c03749fc
